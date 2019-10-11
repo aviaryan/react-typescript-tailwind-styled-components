b5a498532083307889dd7e2e5a027e9d24ff9e5f
Avi Aryan <avi.aryan123@gmail.com>
2019-10-11 15:45:49 +0530

    add prettier
    seems to work fine with TS for now, no conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 const App: React.FC = () => {
-	return (
-		<div className="App App-header">
-			New App
-		</div>
-	);
+	return <div className='App App-header'>New App</div>
 }
 
-export default App;
+export default App
