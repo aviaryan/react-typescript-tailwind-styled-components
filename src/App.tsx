575725acd4e0affef45fffee73cb1364f92a1cf2
Avi Aryan <avi.aryan123@gmail.com>
2019-10-11 15:33:38 +0530

    add editorconfig
    not adding prettier because not sure with TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App App-header">
-      New App
-    </div>
-  );
+	return (
+		<div className="App App-header">
+			New App
+		</div>
+	);
 }
 
 export default App;
